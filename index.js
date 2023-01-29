efe4a83ee495249cb0615e27a2821ba80d42c3e7
Odiiego <odiegopaula@gmail.com>
2023-01-29 04:44:34 -0300

    Adicionei alguns comportamentos com JS.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const navList = Array.from(document.getElementsByClassName("nav-list-item"))
+const navbar = document.getElementById("navbar")
+const sticky = navbar.offsetTop;
+
+function removeActive() {
+    const alreadyActive = document.getElementById("active")
+
+    if (alreadyActive !== null) {
+        alreadyActive.removeAttribute("id")
+    }
+}
+
 navList.forEach((item) => {
     item.addEventListener("click", (e) => {
-        const alreadyActive = document.getElementById("active")
-        if (alreadyActive !== null) {
-            alreadyActive.removeAttribute("id")
-        }
-
+        removeActive()
         e.target.parentElement.id = "active"
     })
 })
+
+console.log(sticky)
+window.onscroll = function () {
+    const i = Math.floor(window.pageYOffset / window.innerHeight)
+
+    Array.from(document.getElementsByClassName("nav-list-link")).forEach((child, index) => {
+        if (index + 1 == i) {
+            child.parentElement.id = "active"
+        }
+    })
+
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky")
+        removeActive()
+    }
+}
