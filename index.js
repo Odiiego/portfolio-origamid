79f6c122cc2d6cac1d1686be6f26bf38d5c913b0
Odiiego <odiegopaula@gmail.com>
2023-01-30 04:38:22 -0300

    Adicionei as seções que faltavam no html.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const navList = Array.from(document.getElementsByClassName("nav-list-item"))
-const navbar = document.getElementById("navbar")
-const sticky = navbar.offsetTop;
+
+navList.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        removeActive()
+        e.target.parentElement.id = "active"
+    })
+})
 
 function removeActive() {
     const alreadyActive = document.getElementById("active")
@@ -10,28 +15,28 @@ function removeActive() {
     }
 }
 
-navList.forEach((item) => {
-    item.addEventListener("click", (e) => {
-        removeActive()
-        e.target.parentElement.id = "active"
-    })
-})
-
-console.log(sticky)
+const navbar = document.getElementById("navbar");
+const navbarOffset = navbar.offsetTop;
 window.onscroll = function () {
-    const i = Math.round(window.pageYOffset / window.innerHeight) - 1
-    console.log(i)
+    const nav = Array.from(document.getElementsByClassName("nav-list-link"))
+    const projetos = document.getElementById("projetos").offsetTop;
+    const cursos = document.getElementById("cursos").offsetTop;
+    const contato = document.getElementById("contato").offsetTop;
+    const yOffset = window.pageYOffset;
 
-    if (i>= 0) {
-        const nav = Array.from(document.getElementsByClassName("nav-list-link"))
-        nav[i].parentElement.id = "active"
-    }
+    removeActive()
 
-    if (window.pageYOffset + 40 >= sticky) {
+    if (window.pageYOffset >= navbarOffset - 40) {
         navbar.classList.add("sticky")
-
     } else {
         navbar.classList.remove("sticky")
-        removeActive()
+    }
+
+    if (yOffset >= (contato * 0.7)) {
+        nav[2].parentElement.id = "active"
+    } else if (yOffset >= (cursos * 0.7)) {
+        nav[1].parentElement.id = "active"
+    } else if (yOffset >= (projetos * 0.7)) {
+        nav[0].parentElement.id = "active"
     }
 }
